d33d4ed033732064ab4fc7d8b2966b7c78fffa4e
Ying Hu <22794111@student.uwa.edu.au>
2025-05-14 20:29:49 +0800

    Backend Development: Add and implement login unit test

---
+++ test/testConfig.py
@@ -0,0 +1,24 @@
+import os
+import unittest
+from app import app
+from db import db
+
+class TestConfig:
+    TESTING = True
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    SECRET_KEY = 'test-secret-key'
+
+class BaseTestCase(unittest.TestCase):
+    def setUp(self):
+        app.config.from_object(TestConfig)
+        self.app = app
+        self.app_context = self.app.app_context()
+        self.app_context.push()
+        self.client = self.app.test_client()
+        db.create_all()
+
+    def tearDown(self):
+        db.session.remove()
+        db.drop_all()
+        self.app_context.pop()

4c04b50dcb0769d48842083c0900097fbf709ff7
S74r-L0rd <aakashko19998@gmail.com>
2025-05-16 15:12:20 +0800

    Frontend and Backend Development: Added a model for storing the user's prediction.

---
+++ models/my_prediction.py
@@ -0,0 +1,12 @@
+from db import db
+from datetime import datetime
+
+class MyPrediction(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+    prediction_data = db.Column(db.Text)  # JSON string
+    summary = db.Column(db.String(255))
+    note = db.Column(db.Text)
+    created_at = db.Column(db.DateTime, default=datetime.utcnow)
+
+    user = db.relationship('User', backref='my_predictions')
